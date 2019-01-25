ed7b83e632f4c520fada100921d5a44e09ecd59b
James Chen <unknownguy2002@gmail.com>
2019-01-25 19:01:20 +0800

    Added config.yml + countdown

--- src/rtp/main.php
+++ src/rtp/main.php
@@ -11,26 +11,36 @@ use pocketmine\utils\TextFormat;
 use pocketmine\level\Position;
 use pocketmine\entity\Effect;
 use pocketmine\entity\EffectInstance;
+use pocketmine\utils\Config;
 class main extends PluginBase implements Listener{
+	/** @var Config */
+    public $Config;
 	public function onEnable() {
+		@mkdir($this->getDataFolder());
+		$this->saveResource("config.yml");
+		$this->Config = new Config($this->getDataFolder() . "config.yml", Config::YAML);
 		$this->getLogger()->info('RTP Enabled');
 	}
 	public function onDisable() {
-		$this->getLogger()->info('[RTP] Disabled');
+		$this->getLogger()->info('RTP Disabled');
 	}
 	public function onCommand(CommandSender $sender, Command $command, string $label, array $args):bool{
 		switch(strtolower($command->getName())) {
 			case 'rtp':
 				if($sender->hasPermission("rtp")){
+					for ($wait = $this->getConfig()->get("waittime") ; $wait > 0 ; $wait--){
+						$sender->sendMessage("Waiting for $wait more second(s)");
+						sleep(1);
+					}
 					$x = mt_rand('-300', '5000');
 					$y = mt_rand('70', '120');
 					$z = mt_rand('-300', '5000');
 					$sender->teleport(new Position($x, $y, $z));
 					$togive = new EffectInstance(Effect::getEffect(11));
-					$togive->setDuration(20*8);
+					$togive->setDuration(20*$this->getConfig()->get("resistsec"));
 					$togive->setAmplifier(20);
 					$sender->addEffect($togive);
-					$sender->sendMessage("§4You were confused and walked to nowhere");
+					$sender->sendMessage($this->getConfig()->get("message"));
 				}
 		}
 		return true;	
